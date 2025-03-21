67b8d62292eb9ec91c30aff95684ee6bcb8b4702
anilkumar <anilkumarchary22@gmail.com>
2025-03-21 18:38:37 +0530

    Added Best seller section component in the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
 import NewArrivals from "../components/Products/NewArrivals";
+import ProductsDetails from "../components/Products/ProductsDetails";
 
 const Home = () => {
   return (
@@ -8,6 +9,10 @@ const Home = () => {
       <Hero />
       <GenderCollectionSection />
       <NewArrivals />
+
+      {/* Best Seller */}
+      <h2 className="text-3xl text-center font-bold mb-4">Best Seller</h2>
+      <ProductsDetails />
     </div>
   );
 };
