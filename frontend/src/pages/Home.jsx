3489b7d81eaf3767533ce2c02fdb096025d5b93d
anilkumar <anilkumarchary22@gmail.com>
2025-03-22 18:26:02 +0530

    Added the features section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,93 @@
 import Hero from "../components/Layout/Hero";
+import FeaturedCollection from "../components/Products/FeaturedCollection";
+import FeaturesSection from "../components/Products/FeaturesSection";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
 import NewArrivals from "../components/Products/NewArrivals";
+import ProductGrid from "../components/Products/ProductGrid";
 import ProductsDetails from "../components/Products/ProductsDetails";
+
+const placeholderProducts = [
+  {
+    _id: 1,
+    name: "Product 1",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=3",
+      },
+    ],
+  },
+  {
+    _id: 2,
+    name: "Product 2",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=4",
+      },
+    ],
+  },
+  {
+    _id: 3,
+    name: "Product 3",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=5",
+      },
+    ],
+  },
+  {
+    _id: 4,
+    name: "Product 4",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=6",
+      },
+    ],
+  },
+  {
+    _id: 5,
+    name: "Product 1",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=7",
+      },
+    ],
+  },
+  {
+    _id: 6,
+    name: "Product 2",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=8",
+      },
+    ],
+  },
+  {
+    _id: 7,
+    name: "Product 3",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=9",
+      },
+    ],
+  },
+  {
+    _id: 8,
+    name: "Product 4",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=10",
+      },
+    ],
+  },
+];
 
 const Home = () => {
   return (
@@ -13,6 +99,14 @@ const Home = () => {
       {/* Best Seller */}
       <h2 className="text-3xl text-center font-bold mb-4">Best Seller</h2>
       <ProductsDetails />
+      <div className="container mx-auto">
+        <h2 className="text-3xl text-center font-bold mb-4">
+          Top Wears for Women
+        </h2>
+        <ProductGrid products={placeholderProducts} />
+      </div>
+      <FeaturedCollection />
+      <FeaturesSection />
     </div>
   );
 };
