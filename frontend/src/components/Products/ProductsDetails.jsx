99b8c9bf8ef392455e3feb9ff77cc76e8f3d92b9
anilkumar <anilkumarchary22@gmail.com>
2025-03-22 16:54:53 +0530

    Added similar products section

--- frontend/src/components/Products/ProductsDetails.jsx
+++ frontend/src/components/Products/ProductsDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { PiSelectionInverseThin } from "react-icons/pi";
 import { toast } from "sonner";
+import ProductGrid from "./ProductGrid";
 
 const selectedProduct = {
   name: "Stylish Jacket",
@@ -22,6 +23,49 @@ const selectedProduct = {
     },
   ],
 };
+
+const similarProducts = [
+  {
+    _id: 1,
+    name: "Product 1",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=3",
+      },
+    ],
+  },
+  {
+    _id: 2,
+    name: "Product 2",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=4",
+      },
+    ],
+  },
+  {
+    _id: 1,
+    name: "Product 3",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=5",
+      },
+    ],
+  },
+  {
+    _id: 1,
+    name: "Product 4",
+    price: 100,
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=6",
+      },
+    ],
+  },
+];
 
 const ProductsDetails = () => {
   const [mainImage, setMainImage] = useState("");
@@ -197,6 +241,12 @@ const ProductsDetails = () => {
             </div>
           </div>
         </div>
+        <div className="mt-20">
+          <h2 className="text-2xl text-center font-medium mb-4">
+            You May Also Like
+          </h2>
+        </div>
+        <ProductGrid products={similarProducts} />
       </div>
     </div>
   );
